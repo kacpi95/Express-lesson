973c38d1dfd358c55e8c17901f8d6db27fa60917
Kacper Grzywacz <kacpergrzywacz2@gmail.com>
2025-08-10 00:23:09 +0200

    add function getData and return data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return;
+  const [posts, setPosts] = useState([]);
+
+  function getData() {
+    fetch('http://localhost:8000/posts')
+      .then((res) => res.json())
+      .then((data) => {
+        setPosts(data);
+      });
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <>
+      <h1>POSTS</h1>
+      <ul>
+        {posts.map((post) => {
+          return <li key={post.id}>{post.title}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
